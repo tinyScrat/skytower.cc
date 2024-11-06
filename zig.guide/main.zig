6a3636af79e20c6a9dbafc42910c1058ce87487f
tinyScrat <tinyScrat@scratty.me>
2024-11-06 23:56:32 +0800

    add note

--- zig.guide/main.zig
+++ zig.guide/main.zig
@@ -9,6 +9,54 @@ test "always succeeds" {
     try expect(true);
 }
 
-test "always fails" {
-    try expect(false);
+// test "always fails" {
+//     try expect(false);
+// }
+
+test "convert" {
+    const a: u64 = 3;
+    var b: u32 = 2;
+    b += 1;
+
+    try expect(a == @as(u64, b));
+}
+
+test "check undefined" {
+    const c = 3;
+    var d: i32 = undefined;
+    d = 1;
+
+    const e: [3]i32 = [3]i32{ 1, 2, 3 }; // array with explictly length
+    //const array = [_]u32{ 1, 2, 2, 4 }; // inferred length
+
+    try expect(c != d and e.len == 3);
+}
+
+test "if statement" {
+    const a = true;
+    var x: u16 = 0;
+    if (a) {
+        x += 1;
+    } else {
+        x += 2;
+    }
+
+    try expect(x == 1);
+}
+
+test "if expression" {
+    const a = true;
+    var x: u16 = 0;
+    x += if (a) 1 else 2;
+    try expect(x == 1);
+}
+
+test "while" {
+    var i: u8 = 2;
+
+    while (i <= 100) {
+        i *= 2;
+    }
+
+    try expect(i == 128);
 }
