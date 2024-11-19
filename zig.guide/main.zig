5331cb971f1c2e4ff68d8e68d625647ebfc0b1d8
tinyScrat <tinyScrat@scratty.me>
2024-11-19 21:11:16 +0800

    add zig

--- zig.guide/main.zig
+++ zig.guide/main.zig
@@ -2,16 +2,22 @@ const std = @import("std");
 const expect = std.testing.expect;
 
 pub fn main() void {
-    std.debug.print("Hello, {s}!\n", .{"world"});
+    const user = User{
+        .power = 9001,
+        .name = "Scrat",
+    };
+
+    std.debug.print("Hello, {s} with power {d}!\n", .{ user.name, user.power });
 }
+
+pub const User = struct {
+    power: u64,
+    name: []const u8,
+};
 
 test "always succeeds" {
     try expect(true);
 }
-
-// test "always fails" {
-//     try expect(false);
-// }
 
 test "convert" {
     const a: u64 = 3;
@@ -59,4 +65,98 @@ test "while" {
     }
 
     try expect(i == 128);
+}
+
+test "while with continue expression" {
+    var sum: u8 = 0;
+    var i: u8 = 1;
+    while (i <= 10) : (i += 1) {
+        sum += i;
+    }
+
+    try expect(sum == 55);
+}
+
+test "while with continue" {
+    var sum: u8 = 0;
+    var i: u8 = 1;
+    while (i <= 3) : (i += 1) {
+        if (i == 2) continue;
+        sum += i;
+    }
+
+    try expect(i == 4);
+}
+
+test "while with break" {
+    var sum: u8 = 0;
+    var i: u8 = 1;
+    while (i <= 3) : (i += 1) {
+        if (i == 2) break;
+        sum += 1;
+    }
+
+    try expect(sum == 1);
+}
+
+test "for" {
+    const array = [_]u8{ 'a', 'b', 'c' };
+
+    for (array, 0..) |character, index| {
+        _ = character;
+        _ = index;
+    }
+
+    for (array) |character| {
+        _ = character;
+    }
+
+    for (array, 0..) |_, index| {
+        _ = index;
+    }
+
+    for (array) |_| {}
+}
+
+fn addFive(a: u32) u32 {
+    return a + 5;
+}
+
+test "addFive" {
+    const y = addFive(2);
+
+    try expect(@TypeOf(y) == u32);
+    try expect(addFive(1) == 6);
+}
+
+fn fibonacci(n: u16) u16 {
+    if (n == 0 or n == 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+test "fibonacci" {
+    const fi = fibonacci(10);
+
+    try expect(fi == 55);
+}
+
+test "try defer" {
+    var u: i16 = 2;
+    {
+        defer u += 5; // defer the execution of the statement until exiting inclosing block
+        try expect(u == 2);
+    }
+
+    try expect(u == 7);
+}
+
+test "multiple defer" {
+    // the execution of multiple defer are in reverse order
+    var n: f32 = 5;
+    {
+        defer n += 2; // execute second
+        defer n /= 2; // execute first
+    }
+
+    try expect(n == 4.5);
 }
